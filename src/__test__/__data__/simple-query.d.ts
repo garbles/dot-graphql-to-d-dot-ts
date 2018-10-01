807f0684172a2010c3f58661422d773640ade9e5
Gabe Scholz <scholz.gabe@gmail.com>
2018-09-30 19:49:19 -0700

    Add graphql-tag export

--- src/__test__/__data__/simple-query.d.ts
+++ src/__test__/__data__/simple-query.d.ts
@@ -1,4 +1,7 @@
+import { DocumentNode } from "graphql";
 import { TypeUtils, Query } from "./simple-schema";
+
+export const MyQuery: DocumentNode; // graphql-tag
 export type MyQueryResult = TypeUtils.Resolve<
   Query,
   { me: { id: true; email: true; age: true; cat: { meow: true } } }
--- src/query-ir-to-source.ts
+++ src/query-ir-to-source.ts
@@ -2,11 +2,16 @@ import prettier from "prettier";
 import { IR } from "./query-to-ir";
 
 export default (irs: IR[], schemaPath: string): string => {
-  const imports = [`import { TypeUtils, Query } from ${JSON.stringify(schemaPath)};`];
+  const imports = [
+    `import { DocumentNode } from 'graphql';`,
+    `import { TypeUtils, Query } from ${JSON.stringify(schemaPath)};`
+  ];
 
   const exports = irs.map(
     ir =>
-      `export type ${ir.name}Result = TypeUtils.Resolve<Query, ${JSON.stringify(ir.selection)}>;`
+      `
+      export const MyQuery: DocumentNode; // graphql-tag
+      export type ${ir.name}Result = TypeUtils.Resolve<Query, ${JSON.stringify(ir.selection)}>;`
   );
 
   const str = [...imports, ...exports].join("\n").replace(/\n\s+\n/g, "\n");
